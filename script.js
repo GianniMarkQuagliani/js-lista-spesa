44c04daa0c180894da728b3d31247e469344a90a
GianniMarkQuagliani <giannimarkquagliani@gmail.com>
2023-09-14 16:12:57 +0200

    [add] I run a while loop

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ let indice = 0;
 
 // Ottiego l'elemento UL dalla pagina HTML
 const listaSpesa = document.getElementById("listaSpesa");
+
+// Eseguo un ciclo while per aggiungere gli elementi alla lista
+while (indice < lista_spesa.length) {
+    // Creo un elemento LI per ogni elemento della lista della spesa
+    const listItem = document.createElement("li");
+    listItem.textContent = lista_spesa[indice];
+    
+    // Aggiungo l'elemento LI alla lista UL
+    listaSpesa.appendChild(listItem);
+    
+    // Incremento l'indice
+    indice++;
+}
