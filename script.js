0b977ae0659adfb35a2f25dd179c836b37df195a
GianniMarkQuagliani <giannimarkquagliani@gmail.com>
2023-09-14 16:05:55 +0200

    [add] I get the UL element from the HTML page

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ const lista_spesa = ["Pane", "Latte", "Uova", "Frutta", "Verdura"];
 
 // Inizializzo un contatore
 let indice = 0;
+
+// Ottiego l'elemento UL dalla pagina HTML
+const listaSpesa = document.getElementById("listaSpesa");
